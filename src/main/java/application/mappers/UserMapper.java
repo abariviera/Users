dbfb0562969de3c76d83ba231948895257358af6
Ana Caroline Bariviera <ana_caroline@live.com>
2017-05-21 17:39:23 -0300

    adicionado o pacote mappers

--- src/main/java/application/mappers/UserMapper.java
+++ src/main/java/application/mappers/UserMapper.java
@@ -1,4 +1,4 @@
-package application.dao;
+package application.mappers;
 
 import java.util.List;
 
--- src/main/java/application/service/user/UserServiceImpl.java
+++ src/main/java/application/service/user/UserServiceImpl.java
@@ -6,8 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
-import application.dao.UserMapper;
 import application.entity.User;
+import application.mappers.UserMapper;
 
 @Service
 public class UserServiceImpl implements UserService {
